24fb9297c70086a6992e36532a92a3db4c8d87c0
badalb <badal.cs@gmail.com>
2018-08-23 12:03:35 +0530

    need to remove this

---
+++ k8s-cluster-services/src/main/resources/cdr.scala
@@ -0,0 +1,8 @@
+val data = sc.textFile("hdfs://localhost:9000/data/CDR.csv")
+val transactions = data.map(_.split(","))
+val keyvaluemap = transactions.map(x => (x(2),x(1).toInt))
+val combineddata = keyvaluemap.combineByKey((v) => (v,1),(acc: (Int,Int),v) => (acc._1 + v,acc._2 + 1), (acc1: (Int,Int), acc2: (Int,Int)) => (acc1._1 + acc2._1,acc2._2 + acc2._2))
+val averagedata = combineddata.map{ case(map,value) => (key,value._1 / value._2.toFloat) }
+val	sortedaverage = averagedata.sortBy(_._2)
+sortedaverage.take(10)
+
